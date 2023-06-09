f844eb65cbaebc4e6cabbab631f02bca91b690a1
Ludovic Dupre <dupreludovic1@gmail.com>
2023-06-09 07:18:39 +0200

    first commit

---
+++ src/main/java/com/example/masterclass_exo/MasterclassExoApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterclass_exo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterclassExoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterclassExoApplication.class, args);
+	}
+
+}
